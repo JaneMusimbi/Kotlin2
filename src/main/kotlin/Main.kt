81f3d12c17689bf41cd98c1052de0c2387a5ba32
JaneMusimbi <janemusimbi@gmail.com>
2022-02-20 21:20:00 -0500

    kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main(){
+    myFunction("Jane")
+    modulus(7,3)
+    sum(70,34,68,100)
+    playMe("basketball")
+}
+
+
+fun myFunction(name: String){
+    println("Hello Jane")
+
+}
+
+fun modulus(num1:Int , num2:Int): Int{
+    var x = (num1 % num2 )
+    println(x)
+    return x
+
+}
+
+fun  sum(num1:Int, num2:Int, num3:Int, num4:Int): Int{
+    var w =( num1+ num2+ num3+ num4)
+    println(w)
+    return w
+
+}
+
+fun playMe(activity:String){
+    println("basketball")
+
+
+}
